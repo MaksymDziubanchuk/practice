7b9383ce870725228772386c5d35e4197bd1ddd9
MaksymDziubanchuk <94477065+MaksymDziubanchuk@users.noreply.github.com>
2022-09-27 20:54:59 +0300

    start RESTful

---
+++ routers/postsRouter.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const router = express.Router();
+
+let posts = [
+    {id: '1', topic: 'test1', text: 'test text1'},
+    {id: '2', topic: 'test2', text: 'test text2'},
+    {id: '3', topic: 'test3', text: 'test text3'},
+]; 
+
+router.get('/', (req, res) => {
+    res.json({posts, status: 'success'});
+})
+
+router.get('/:id', (req, res) => {
+    const id = req.params.id;
+
+    const [post] = posts.filter(item => item.id === id);
+    if(post){
+       return res.json({post, status: 'success'});
+    }
+    return  res.status(404).json({status: 'undefinded'});
+})
+
+router.post('/', (req, res) => {
+    const {
+        topic,
+        text
+    } = req.body;
+
+    posts.push({
+        id: new Date,
+        topic,
+        text
+    });
+    res.json({posts, status: 'success'});
+})
+
+router.put('/:id', (req, res) => {
+    const id = req.params.id;
+    const {
+        topic,
+        text
+    } = req.body;
+
+    const [post] = posts.filter(item => item.id === id);
+    if(post){
+        posts.forEach(item => {
+            if(item.id === id){
+                item.topic = topic;
+                item.text = text;
+            }
+        });
+       return res.json({posts, status: 'success'});
+    }
+    return  res.status(404).json({status: 'undefinded'});
+
+})
+
+router.delete('/:id', (req, res) => {
+    const id = req.params.id;
+    const [post] = posts.filter(item => item.id === id);
+    if(!post){
+        return  res.status(404).json({status: 'undefinded'});
+    }
+    posts = posts.filter(item => item.id !== id); 
+    res.json({posts, status: 'success'});
+})
+
+module.exports = {
+    postsRouter: router
+}
+ 
--- server.js
+++ server.js
@@ -1,84 +1,21 @@
-const express = require("express");
-const morgan = require("morgan");
-const axios= require('axios').default;
+const express = require('express')
+const morgan = require("morgan")
 require('dotenv').config();
+
 const app = express();
 
-const {router} = require('./booksRouter')
+const {postsRouter} = require('./routers/postsRouter')
 
-const PORT = process.env.PORT ?? 8081;
-const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
-const language = process.env.LANGUAGE;
-
-const baseUrl = "http://api.weatherbit.io/v2.0/current";
-
+const PORT = process.env.PORT || 8081;
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use(express.static('public'));
-app.use(morgan('tiny'));
-app.use(router);
+app.use(morgan('dev'));
+app.use('/api/posts', postsRouter);
 
-app.get('/api/weather', async (req, res) => {
-    try {
-        const {
-            latitude,
-            longitude
-        } = req.query;
-
-        if (!latitude) {
-           return  res.status(400).json({message: 'latitude parameter is requaired'})
-        };
-
-        if (!longitude) {
-            return res.status(400).json({message: 'longitude parameter is requaired'})
-        };
- 
-        const params = {
-            key: WEATHER_API_KEY,
-            lang: language,
-            lat: latitude,
-            lon: longitude
-        };
-
-        await axios.get(baseUrl, {params}).then(function (response) {
-
-        const [{city_name, wind_spd, app_temp, datetime, temp, weather}] = response.data.data;
-            res.json({city_name, wind_spd, app_temp, datetime, temp, weather})
-          })
-          .catch(function (error) {
-            res.status(500).json({message: error.message})
-          })
-       
-    } catch (error) {
-       res.status(500).json({message: error.message})
-    }
-})
-
-
-// app.use((req, res, next) => {
-//     // some code without res. or with RETURN and it will be end of response
-//     next();
-// }) 
-
-// app.post('/home', (req, res) => {
-//  if (!req.body.test){
-//     return res.status(400).json({status: 'TEST parameter is required'})
-//  }
-//     res.json({js: "post request", body: req.body});
-
-// })
-
-
- 
 app.listen(PORT, (err) => {
-    if(err) {
-        console.log("Error at eserver launch:", err);
-    }
+    if(err)         console.log("Error at eserver launch:", err);
+    
     console.log(`Server works at port ${PORT}`)
 });
-
-
-
 
 
